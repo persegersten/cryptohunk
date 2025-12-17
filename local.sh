1019a9a2daf1ab495573dda655aeef4a5eeb2c79
Per Segersten <persegersten@gmail.com>
2025-12-17 16:50:33 +0100

    Clean up

--- local.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export SCHEDULE_FORCE_RUN=true
-export TRADE_DRY_RUN=true
-export SKIP_DOWNLOAD_HISTORY=false
-# export FIXIE_SOCKS_HOST="fixie:J0duPrq3gn6EEdi@bici.usefixie.com:1080"
-
-python src/analyse_and_trade_three_assets_weighted.py
