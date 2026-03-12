88b418b3a3acf00fd0cba21e7bd9951767e7694c
Per Segersten <persegersten@gmail.com>
2026-03-12 15:34:13 +0100

    Analyse config

--- analyse.sh
+++ analyse.sh
@@ -2,7 +2,7 @@
 # Enkel wrapper för att köra CryptoHunk2.0 från kommandoraden.
 # Exempel:
 export CURRENCIES="BNB,ETH,SOL"
-export DATA_AREA_ROOT_DIR="/home/perseg/dev/cryptotrader/tmp/cryptohunk_data"
+export DATA_AREA_ROOT_DIR="/tmp/cryptohunk_data"
 export CURRENCY_HISTORY_PERIOD="1h"
 export CURRENCY_HISTORY_NOF_ELEMENTS="1000"
 
