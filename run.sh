391285261d3db7711ad10e42495d6684517fb195
Per Segersten <persegersten@gmail.com>
2026-04-27 07:33:51 +0200

    logfile

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 export CURRENCIES="BTC,ETH,SOL"
 export BINANCE_TRADING_URL="https://www.binance.com/api/v3/order"
 export DATA_AREA_ROOT_DIR="/tmp/cryptohunk_data"
+export LOG_FILE="tmp/run.log"
 export CURRENCY_HISTORY_PERIOD="1h"
 export CURRENCY_HISTORY_NOF_ELEMENTS="1000"
 export TRADE_THRESHOLD="10"
@@ -18,4 +19,15 @@ set -euo pipefail
 PYTHON=${PYTHON:-python3}
 
 echo "Kör AssertEnv och main..."
-$PYTHON -m src.main "$@" --clean-data --collect-data --run-ta --rebalance-portfolio --create-trade-plan --execute-trades --backtest --visualize --ftp-upload
+$PYTHON -m src.main "$@" --clean-data --collect-data --run-ta --rebalance-portfolio --create-trade-plan --execute-trades --backtest --visualize --ftp-upload 2>&1 | tee "$LOG_FILE"
+
+
+exit_code=${PIPESTATUS[0]}
+set -e
+
+curl --ftp-create-dirs \
+  --user "$FTP_USERNAME:$FTP_PASSWORD" \
+  --upload-file "$LOG_FILE" \
+  "ftp://$FTP_HOST/$FTP_DIR/run.log"
+
+exit "$exit_code"
